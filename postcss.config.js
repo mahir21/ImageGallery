caeb313772d418fa5acc3150ab2359b906f3ee4d
mahir999 <mahirjeet@gmail.com>
2022-09-18 10:43:44 +0600

    first commit image gallery

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+const autoprefixer = require('autoprefixer');
+const tailwindcss = require('tailwindcss');
+
+module.exports = {
+   
+    plugins: [
+        tailwindcss('./tailwind.js'),
+        require(autoprefixer)
+    ]
+    
+
+
+}
